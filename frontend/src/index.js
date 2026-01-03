7adc8303efd5a087697e20135b8ad4fefabdc92f
Samira <erp.atmanis@gmail.com>
2026-01-03 22:03:21 +0100

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // <-- ajouter
+import App from './App.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/webiw">
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter basename="/webiw">  
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
